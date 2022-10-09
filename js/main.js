90d2c5c69239238e996e9cb81f932a8256483a5b
Alexandr136 < email>
2022-10-09 21:01:20 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 function getRandomInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 }
 
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
 
 getRandomInteger();
 checkStringLength();
+
+const COUNT = 25;
+
+const DISCRIPTIONS = [
+  'Красивые котики',
+  'Неугомонный малыш',
+  'Пухляш',
+  'Молодая семейка',
+  'Хоба'
+];
+
+const creatRandomItem = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
+const createPost = (postId) => {
+  const post = [];
+
+  for (let i = 1; i <= postId; i++) {
+    post.push({
+      id: i,
+      url: `photos/ ${ i } .jpg`,
+      description: creatRandomItem(DISCRIPTIONS),
+      likes: getRandomInteger(15, 200),
+      comments: getRandomInteger(0, 200),
+    });
+  }
+
+  return post;
+};
+
+createPost();
