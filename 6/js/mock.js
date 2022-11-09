a889f66fe510bd984f91754d9b7218005310e750
Keks <keks@htmlacademy.ru>
2022-11-09 16:53:30 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/mock.js
@@ -0,0 +1,23 @@
+import {getRandomInteger, creatRandomItem} from './util.js';
+
+const COUNT = 25;
+
+const DISCRIPTIONS = [
+  'Красивые котики',
+  'Неугомонный малыш',
+  'Пухляш',
+  'Молодая семейка',
+  'Хоба'
+];
+
+const createPost = (id) => ({
+  id: id,
+  url: `photos/${ id }.jpg`,
+  description: creatRandomItem(DISCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: getRandomInteger(0, 200),
+});
+
+const similarPosts = Array.from({length: COUNT}, (item, index) => createPost(index + 1));
+
+export {similarPosts};
