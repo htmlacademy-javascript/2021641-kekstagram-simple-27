a2ffbe131c3c6670e85df521f57ea20c09649056
Alexandr136 < email>
2022-11-21 20:00:43 +0300

    удалил моки

--- js/util.js
+++ js/util.js
@@ -1,17 +1,5 @@
-function getRandomInteger (a, b) {
-  if (a < 0 || b < 0) {
-    return NaN;
-  }
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-
-const creatRandomItem = (arr) => arr[getRandomInteger(0, arr.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const getTemplateElement = (parent, templateId, elementClass) => parent.querySelector(`#${templateId}`).content.querySelector(`.${elementClass}`);
 
-export {getRandomInteger, creatRandomItem, isEscapeKey, getTemplateElement};
+export {isEscapeKey, getTemplateElement};
